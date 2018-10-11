d8b3ad0b9f3fa041c5c851fbe04a8ec119571196
siddharth6 <siddharth@readybytes.in>
2018-10-11 15:19:09 -0400

    Rectification in KeyMap.java

--- KeyMap.java
+++ KeyMap.java
@@ -1,39 +1,38 @@
 import java.util.*;
 
 public class KeyMap{
-boolean CapsLockToggle  =   false;
+boolean CapsLockToggle  =   false,isApressed = false;
     public static void main(String args[]){
         System.out.println("Enter the input");
         Scanner input   =  new Scanner(System.in);
         String inputone="";
                 inputone    =   input.nextLine();
-        
         KeyMap keyMap   =   new KeyMap();
-        System.out.println(keyMap.pressAForCapsLock(inputone));    
+        char[] chinput = keyMap.pressAForCapsLock(inputone);
+        for(char ch:chinput){
+            System.out.print(ch);    
+        }
     }
-    public String pressAForCapsLock(String input){
+    public char[] pressAForCapsLock(String input){
            char[] inputarray =  input.toCharArray();         
            char[] newInputArray = new char[inputarray.length]; 
            for(int i =0;i<input.length();i++){
                if(inputarray[i]=='a'){
+                   isApressed=true;
                         if(CapsLockToggle){
                             CapsLockToggle=false;
                         }
                         else{
-                                CapsLockToggle=true;
+                            CapsLockToggle=true;
                         }
                 }
                 else if(CapsLockToggle){
                     newInputArray[i]  = Character.toUpperCase(inputarray[i]);
                 }
-                else if(!CapsLockToggle){
-                    newInputArray[i] = Character.toLowerCase(inputarray[i]);
-                }
                 else{
                     newInputArray[i] = inputarray[i];
                 }
             }
-            System.out.println(newInputArray);
-            return "asdf";
+            return newInputArray;
         }
 }
